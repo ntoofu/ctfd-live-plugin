add8e4ca3a185493e6518b6afa7b492042e57504
ntoofu <ntoofu@users.noreply.github.com>
2023-10-14 11:23:01 +0900

    Fix code format

--- __init__.py
+++ __init__.py
@@ -5,29 +5,27 @@ from flask import current_app, render_template, request
 from flask_restx import Namespace, Resource
 from sqlalchemy import func
 
-from CTFd.models import db, Users
-from CTFd.utils.user import get_current_user
-from CTFd.plugins import register_plugin_asset, override_template
 from CTFd.api import CTFd_API_v1
-from CTFd.utils.decorators import (
-    admins_only,
-    during_ctf_time_only,
-)
+from CTFd.models import Users, db
+from CTFd.plugins import override_template, register_plugin_asset
+from CTFd.utils.decorators import admins_only, during_ctf_time_only
+from CTFd.utils.user import get_current_user
 
 
 class ChallengeViews(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), index=True)
-    challenge_id = db.Column(db.Integer, db.ForeignKey('challenges.id', onupdate="CASCADE", ondelete="CASCADE"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), index=True)
+    challenge_id = db.Column(
+        db.Integer,
+        db.ForeignKey("challenges.id", onupdate="CASCADE", ondelete="CASCADE"),
+    )
     date = db.Column(db.DateTime, default=datetime.datetime.utcnow, index=True)
 
     def __init__(self, *args, **kwargs):
         super(ChallengeViews, self).__init__(**kwargs)
 
 
-live_namespace = Namespace(
-    "challenges", description="Endpoint used by 'live' plugin"
-)
+live_namespace = Namespace("challenges", description="Endpoint used by 'live' plugin")
 
 
 @live_namespace.route("/challenge_views")
@@ -35,23 +33,32 @@ class LiveChallengeViews(Resource):
     @admins_only
     @during_ctf_time_only
     def get(self):
-        subquery = db.session.query(ChallengeViews,
-                                    func.rank().over(
-                                        order_by=ChallengeViews.date.desc(),
-                                        partition_by=ChallengeViews.user_id).label('order')
-                                    ).subquery()
-        views = db.session.query(subquery, Users.team_id, Users.name).filter(subquery.c.order==1).join(Users, subquery.c.user_id == Users.id)
+        subquery = db.session.query(
+            ChallengeViews,
+            func.rank()
+            .over(
+                order_by=ChallengeViews.date.desc(), partition_by=ChallengeViews.user_id
+            )
+            .label("order"),
+        ).subquery()
+        views = (
+            db.session.query(subquery, Users.team_id, Users.name)
+            .filter(subquery.c.order == 1)
+            .join(Users, subquery.c.user_id == Users.id)
+        )
         return {
-                "success": True,
-                "data": [
-                    {"user_id": v.user_id,
-                     "user_name": v.name,
-                     "team_id": v.team_id,
-                     "challenge": v.challenge_id,
-                     "date": v.date.isoformat()}
-                    for v in views
-                    ]
+            "success": True,
+            "data": [
+                {
+                    "user_id": v.user_id,
+                    "user_name": v.name,
+                    "team_id": v.team_id,
+                    "challenge": v.challenge_id,
+                    "date": v.date.isoformat(),
                 }
+                for v in views
+            ],
+        }
 
 
 def add_challenge_views(user_id, challenge_id):
@@ -63,15 +70,18 @@ def add_challenge_views(user_id, challenge_id):
 
 def send_message(user_id, challenge_id, msg):
     print(f"{msg} {challenge_id} by {user_id}")  # save activity on DB & send message
-    current_app.events_manager.publish(data={"user_id": user_id, "challenge_id": challenge_id, "message": msg}, type="live")
+    current_app.events_manager.publish(
+        data={"user_id": user_id, "challenge_id": challenge_id, "message": msg},
+        type="live",
+    )
     return
 
 
 def load(app):
     app.db.create_all()
 
-    challenge_func_name = 'api.challenges_challenge'
-    attempt_func_name = 'api.challenges_challenge_attempt'
+    challenge_func_name = "api.challenges_challenge"
+    attempt_func_name = "api.challenges_challenge_attempt"
     challenge_func_orig = app.view_functions[challenge_func_name]
     attempt_func_orig = app.view_functions[attempt_func_name]
 
@@ -109,12 +119,15 @@ def load(app):
     app.view_functions[challenge_func_name] = challenge_func
     app.view_functions[attempt_func_name] = attempt_func
 
-    register_plugin_asset(app, asset_path='/plugins/live/assets/main.js')
-    register_plugin_asset(app, asset_path='/plugins/live/assets/main.css')
-    override_template('live.html', '{% extends "base.html" %}{% block scripts %}<script type="text/javascript" src="/plugins/live/assets/main.js"></script><link rel="stylesheet" type="text/css" href="/plugins/live/assets/main.css">{% endblock %}{% block content %}<h1>Live Scoreboard</h1>{{content}}<div id="ctfd-live-plugin-container"></div>{% endblock %}')
+    register_plugin_asset(app, asset_path="/plugins/live/assets/main.js")
+    register_plugin_asset(app, asset_path="/plugins/live/assets/main.css")
+    override_template(
+        "live.html",
+        '{% extends "base.html" %}{% block scripts %}<script type="text/javascript" src="/plugins/live/assets/main.js"></script><link rel="stylesheet" type="text/css" href="/plugins/live/assets/main.css">{% endblock %}{% block content %}<h1>Live Scoreboard</h1>{{content}}<div id="ctfd-live-plugin-container"></div>{% endblock %}',
+    )
 
-    @app.route('/admin/live', methods=['GET'])
+    @app.route("/admin/live", methods=["GET"])
     def view_live():
-        return render_template('live.html', content='')
+        return render_template("live.html", content="")
 
     CTFd_API_v1.add_namespace(live_namespace, "/live")
